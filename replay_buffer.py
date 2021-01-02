cac6d1009381070a5d4a382adb85dc62d5d1f448
Programmer <info2016frei@gmail.com>
2021-01-02 21:56:11 +0100

    init

--- replay_buffer.py
+++ replay_buffer.py
@@ -9,18 +9,14 @@ class ReplayBuffer(object):
     def __init__(self, obs_shape, action_shape, capacity, device):
         self.capacity = capacity
         self.device = device
-
-
         self.obses = np.empty((capacity, *obs_shape), dtype=np.float32)
         self.next_obses = np.empty((capacity, *obs_shape), dtype=np.float32)
         self.actions = np.empty((capacity, *action_shape), dtype=np.int64)
         self.rewards = np.empty((capacity, 1), dtype=np.float32)
         self.not_dones = np.empty((capacity, 1), dtype=np.float32)
         self.not_dones_no_max = np.empty((capacity, 1), dtype=np.float32)
-
         self.idx = 0
         self.full = False
-        self.k = 0
 
     def __len__(self):
         return self.capacity if self.full else self.idx
@@ -50,37 +46,6 @@ class ReplayBuffer(object):
         rewards = torch.as_tensor(self.rewards[idxs], device=self.device)
         not_dones_no_max = torch.as_tensor(self.not_dones_no_max[idxs], device=self.device)
         return obses, actions, rewards, next_obses, not_dones_no_max
-
-
-    def expert_policy(self, batch_size):
-        idxs = np.random.randint(0, self.capacity if self.full else self.idx, size=batch_size)
-    
-        obses = self.obses[idxs]
-        next_obses = self.next_obses[idxs]
-
-        obses = torch.as_tensor(obses, device=self.device)
-        next_obses = torch.as_tensor(next_obses, device=self.device)
-        actions = torch.as_tensor(self.actions[idxs], device=self.device)
-        dones = torch.as_tensor(self.not_dones_no_max[idxs], device=self.device)
-
-        return obses, next_obses, actions, dones
-
-    def add_expert(self, obs, action, reward, next_obs, done, done_no_max):
-        
-        for a in range(4):
-            self.k +=1
-            np.copyto(self.obses[self.idx], obs)
-            np.copyto(self.actions[self.idx], a)
-            np.copyto(self.rewards[self.idx], reward)
-            np.copyto(self.next_obses[self.idx], next_obs)
-            np.copyto(self.not_dones[self.idx], not done)
-            np.copyto(self.not_dones_no_max[self.idx], not done_no_max)
-            
-            self.idx = (self.idx + 1) % self.capacity
-            self.full = self.full or self.idx == 0
-
-
-
 
     def save_memory(self, filename):
         """
