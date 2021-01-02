478e33a3f8b652c2fc8968353db2e0b674df96f1
Programmer <info2016frei@gmail.com>
2021-01-02 21:54:24 +0100

    init

--- model.py
+++ model.py
@@ -20,27 +20,29 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc1 = nn.Linear(state_size + action_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.fc3 = nn.Linear(fc2_units, 1)
         
-        self.fc4 = nn.Linear(state_size, fc1_units)
+        self.fc4 = nn.Linear(state_size + action_size, fc1_units)
         self.fc5 = nn.Linear(fc1_units, fc2_units)
-        self.fc6 = nn.Linear(fc2_units, action_size)
+        self.fc6 = nn.Linear(fc2_units, 1)
 
-    def forward(self, state):
+    def forward(self, state, action):
         """Build a network that maps state -> action values."""
-        x1 = F.relu(self.fc1(state))
+        xu = torch.cat([state, action], dim=1)
+        x1 = F.relu(self.fc1(xu))
         x1 = F.relu(self.fc2(x1))
         x1 = self.fc3(x1)
         
-        x2 = F.relu(self.fc4(state))
+        x2 = F.relu(self.fc4(xu))
         x2 = F.relu(self.fc5(x2))
         x2 = self.fc6(x2)
         return x1, x2
 
-    def Q1(self, state):
-        x1 = F.relu(self.fc1(state))
+    def Q1(self, state, action):
+        xu = torch.cat([state, action], dim=1)
+        x1 = F.relu(self.fc1(xu))
         x1 = F.relu(self.fc2(x1))
         x1 = self.fc3(x1)
         return x1
@@ -65,19 +67,10 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
-        self.softmax_layer = nn.Softmax( dim=1) 
-        self.clip = clip
     
     def forward(self, state):
         """Build a network that maps state -> action values."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         logits = self.fc3(x)
-        mu = torch.argmax(logits, dim=1)
-        action_prob = self.softmax_layer(logits) 
-        action_prob = action_prob + torch.finfo(torch.float32).eps
-        log_action_prob = torch.log(action_prob)
-        log_action_prob = torch.clamp(log_action_prob, min= self.clip, max=0)
-        policy_dist = Categorical(logits=logits)
-        pi = policy_dist.sample() 
-        return mu, pi, action_prob, log_action_prob, logits
+        return logits
